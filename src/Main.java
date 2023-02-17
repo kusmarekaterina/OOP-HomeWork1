653fa7dad3830b18ae5f8d0139fd432fcbeb5772
Ekaterina <aliya_kreins@mail.ru>
2023-02-17 20:58:10 +0400

    Finished homework2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import transport.Car;
+
 public class Main {
-    public static Car [] cars = new Car [10];
+    public static Car[] cars = new Car [10];
 
     public static void createCarBook() {
         cars = new Car []{
-        new Car("Granta", "", "жёлтого", "Россия", 2015, 1.7),
-        new Car("Audi", "A8 50 L TDI quattro", "чёрного", "Германия", 2020, 3.0),
-        new Car("BMW", "Z8", "чёрного", "Германия", 2021, 3),
-        new Car("Kia", "Sportage 4-го поколения", "красного", "Южная Корея", 2018, 2.4),
-        new Car("Hyundai", "Avante", null, "Южная Корея", 2016, 1.6),
+        new Car("Granta", "", "жёлтого", "Россия", 2015, 1.7, null, "", "", 2, "" ),
+        new Car("Audi", "A8 50 L TDI quattro", "чёрного", "Германия", 2020, 3.0,null, "", "", 2,""),
+        new Car("BMW", "Z8", "чёрного", "Германия", 2021, 3, null, "", "", 2, ""),
+        new Car("Kia", "Sportage 4-го поколения", "красного", "Южная Корея", 2018, 2.4, null, "", "", 2,""),
+        new Car("Hyundai", "Avante", null, "Южная Корея", 2016, 1.6, null, "", "", 2,""),
         };
     }
 
@@ -19,6 +21,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-            printCarBook();
+
+        printCarBook();
     }
 }
